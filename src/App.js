0ea448a70a42b5831644a60d6f67c171b5198ab8
Kwon Moon <kwonmoon@gmail.com>
2019-03-14 06:53:25 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import TourList from "./components/TourList";
+
+import "./App.scss";
+
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <Navbar />
+        <TourList />
+      </main>
+    );
+  }
+}
+
+export default App;
